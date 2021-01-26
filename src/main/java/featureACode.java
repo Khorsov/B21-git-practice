d22f72e2e477ae4d4ac607b7e5d067aafe16e7cd
Khorsov <hhorsovv@gmail.com>
2021-01-26 14:26:08 -0500

    add to branch

--- src/main/java/featureACode.java
+++ src/main/java/featureACode.java
@@ -1,7 +1,7 @@
 public class featureACode {
     public static void main(String[] args) {
         System.out.println("Awesome work for A started");
-
+        System.out.println("Additional scripts ");
 
     }
 }
